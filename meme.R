16ebe4dd6bff86564d553f58784daa9b8513c5a7
bbangcong <101078437+bbangcong@users.noreply.github.com>
2022-03-21 02:45:51 +1300

    Add files via upload

--- meme.R
+++ meme.R
@@ -51,5 +51,6 @@ third_row <- c(rotten_potato, third_text) %>%
 c(first_row, second_row, third_row) %>%
   image_append(stack = TRUE)
 
-image_write(meme, "my_meme.png")
+c(first_row, second_row, third_row) %>%
+  image_write(meme, "my_meme.png")
 
